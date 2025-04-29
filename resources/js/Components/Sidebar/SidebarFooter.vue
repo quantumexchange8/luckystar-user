9d95e32431922758397989ef2c188c04a76a57e0
Ang Le Xuan <90453020+AngLeXuan123@users.noreply.github.com>
2025-04-29 14:31:43 +0800

    fix: sidebar footer

--- resources/js/Components/Sidebar/SidebarFooter.vue
+++ resources/js/Components/Sidebar/SidebarFooter.vue
@@ -8,14 +8,17 @@ const user = usePage().props.auth.user;
 </script>
 
 <template>
-    <div class="flex-shrink-0 p-3 sticky">
+    <div 
+        class="flex-shrink-0 p-3 sticky"
+    >       
         <Link
             :href="route('profile.edit')"
             :class="[
                 'flex items-center gap-3 self-stretch group select-none cursor-pointer transition-colors py-3 px-7',
+                sidebarState.isOpen || sidebarState.isHovered ? 'flex-row gap-3 px-7' : 'flex-col gap-1 px-2',
                 {
-                    'text-surface-950 dark:text-white hover:text-primary hover:bg-primary-50 dark:hover:bg-surface-800 dark:hover:text-primary-500': !route().current('profile.edit'),
-                    'text-primary bg-primary-50 dark:bg-surface-800 hover:bg-primary-100': route().current('profile.edit'),
+                    'text-surface-950 dark:text-white hover:text-primary hover:bg-primary-50 dark:hover:bg-surface-800 dark:hover:text-primary-500 rounded-lg': !route().current('profile.edit'),
+                    'text-primary bg-primary-50 dark:bg-surface-800 hover:bg-primary-100 rounded-lg': route().current('profile.edit'),
                 },
             ]"
         >
@@ -30,12 +33,12 @@ const user = usePage().props.auth.user;
                 shape="circle"
             />
 
-            <div class="flex flex-col text-sm">
+            <div v-if="sidebarState.isOpen || sidebarState.isHovered" class="flex flex-col text-sm">
                 <div class="flex items-center gap-1 font-semibold">
                     <span class="max-w-28 truncate">{{ user.username }}</span>
                 </div>
                 <span class="text-xs text-surface-500 max-w-36 truncate">{{ user.email }}</span>
             </div>
-            </Link>
+        </Link>
     </div>
 </template>
