026f3e3980770db8737b33e03624039418af09e1
Ang Le Xuan <90453020+AngLeXuan123@users.noreply.github.com>
2025-05-13 16:13:29 +0800

    fix: kyc upload

--- resources/js/Pages/Profile/Detail/Partials/KycVerification.vue
+++ resources/js/Pages/Profile/Detail/Partials/KycVerification.vue
@@ -153,34 +153,18 @@ const getSeverity = (status) => {
                 </div>
 
                 <div v-else class="w-full">
-                    <div class="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-5 w-full">
-                        <div class="flex flex-col gap-1 items-start self-stretch">
-                            <InputLabel for="front_identity">{{ $t('public.front_identity' )}}</InputLabel>
-                            <div
-                                class="flex flex-col gap-3 items-center self-stretch px-5 py-8 rounded-md border-2 border-dashed transition-colors duration-150 bg-surface-50 dark:bg-surface-950 border-surface-300 dark:border-surface-600"
-                            >
-                                <Image
-                                    role="presentation"
-                                    alt="front_identity_image"
-                                    :src="front_identity_image"
-                                    preview
-                                    imageClass="w-full object-contain h-40"
-                                />
-                            </div>
-                        </div>
-                        <div class="flex flex-col gap-1 items-start self-stretch">
-                            <InputLabel for="front_identity">{{ $t('public.back_identity' )}}</InputLabel>
-                            <div
-                                class="flex flex-col gap-3 items-center self-stretch px-5 py-8 rounded-md border-2 border-dashed transition-colors duration-150 bg-surface-50 dark:bg-surface-950 border-surface-300 dark:border-surface-600"
-                            >
-                                <Image
-                                    role="presentation"
-                                    alt="back_identity_image"
-                                    :src="back_identity_image"
-                                    preview
-                                    imageClass="w-full object-contain h-40"
-                                />
-                            </div>
+                    <div class="flex flex-col gap-1 items-start self-stretch">
+                        <InputLabel for="front_identity">{{ $t('public.front_identity' )}}</InputLabel>
+                        <div
+                            class="flex flex-col gap-3 items-center self-stretch px-5 py-8 rounded-md border-2 border-dashed transition-colors duration-150 bg-surface-50 dark:bg-surface-950 border-surface-300 dark:border-surface-600"
+                        >
+                            <Image
+                                role="presentation"
+                                alt="residency_image"
+                                :src="residency_image"
+                                preview
+                                imageClass="w-full object-contain h-40"
+                            />
                         </div>
                     </div>
                 </div>
